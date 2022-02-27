4d906a4d1f4202848504d78ac39d0aeaa9c94900
Roman Litskevich <roman_litskevich@epam.com>
2022-02-27 18:52:16 +0200

    refactor: delete timeout for server

--- server.js
+++ server.js
@@ -3,14 +3,8 @@
 const app = require('express')();
 const images = require('./src/images.json');
 
-const SERVER_TIMEOUT = 1000;
-
 app.get('/images', ({ query }, res) => {
   const i = query.limit ? images.slice(0, parseInt(query.limit)) : images;
-
-  // setTimeout(() => {
-  //   return res.status(200).json(i);
-  // }, SERVER_TIMEOUT);
 
   return res.status(200).json(i);
 });
